6b5676176892c053ddd24d3a78eef9446ab5b98c
satish <satish@skybase.com.np>
2022-08-15 10:41:21 +0545

    Update Package.json

--- src/index.js
+++ src/index.js
@@ -10,13 +10,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use("/api/v1", v1Router);
+app.use('/', (req, res, next) => {
+    res.send("404 NOT FOUND")
+})
 
 const run = async () => {
     await dbConnection.createConnection()
     .then( connectionEstablished => {
         if (connectionEstablished) {
             app.listen(PORT, () => {
-                console.log("API is listening on port ${PORT}");
+                console.log(`API is listening on port ${PORT}`);
             });
         }
     }
