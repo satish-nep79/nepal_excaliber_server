a5b7dcc94a2cbb4486d6216efbecb774ee74df66
satish <satish@skybase.com.np>
2022-09-09 09:47:13 +0545

    Update Database

--- src/database/db.js
+++ src/database/db.js
@@ -1,25 +1,25 @@
 const Sequelize = require("sequelize");
 
-// const sequelizeDb = new Sequelize(
-//     "b2wzj8jw6fqkhchlfuof",
-//     "uomvxe14tnin7ujt",
-//     "hqodN5f2Zqb063Zt18EG",
-//     {
-//         dialect: 'mysql',
-//         host: "b2wzj8jw6fqkhchlfuof-mysql.services.clever-cloud.com",
-//         port: 3306
-//     }
-// );
-
 const sequelizeDb = new Sequelize(
-    "nepal_xcaliber",
-    "root",
-    "",
+    "b2wzj8jw6fqkhchlfuof",
+    "uomvxe14tnin7ujt",
+    "hqodN5f2Zqb063Zt18EG",
     {
         dialect: 'mysql',
-        host: "localhost",
+        host: "b2wzj8jw6fqkhchlfuof-mysql.services.clever-cloud.com",
+        port: 3306
     }
 );
+
+// const sequelizeDb = new Sequelize(
+//     "nepal_xcaliber",
+//     "root",
+//     "",
+//     {
+//         dialect: 'mysql',
+//         host: "localhost",
+//     }
+// );
 
 module.exports = {
     sequelizeDb
